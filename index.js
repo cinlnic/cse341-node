7c5b8749c26dfa650a53f1c7e0e2b80e3e46a4b0
Cindi Nichols <cindi.l.nichols@gmail.com>
2021-09-23 12:48:18 -0600

    code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const express = require('express');
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -20,4 +21,4 @@ app.use((req, res, next) => {
     res.status(404).render('404', { pageTitle: 'Page Not Found', path: req.url });
 });
 
-app.listen(5000);
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
